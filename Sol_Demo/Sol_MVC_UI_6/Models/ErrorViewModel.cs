e3ddb36455c53b798565139b7a19447b69a99e5d
Kishor Naik <kishor.naik011.net@gmail.com>
2022-03-22 21:06:23 +0530

    Code Commit : Library working on Asp.net Core MVC 6 too

---
+++ Sol_Demo/Sol_MVC_UI_6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sol_MVC_UI_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
